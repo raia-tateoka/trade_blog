c11b9ff2fdd4cdd6fef051c8455676b219a85559
_gorilla_ <t.workout.r@gmail.com>
2024-06-23 22:29:32 +0900

    画面の表示処理

--- src/app/lib/notion.ts
+++ src/app/lib/notion.ts
@@ -20,6 +20,10 @@ interface NotionPost {
   author: string
 };
 
+/**
+ * ブログ一覧の情報取得
+ * @returns 
+ */
 export async function getAllPosts(): Promise<NotionPost[]> {
   const response = await notion.databases.query({
     database_id: process.env.DATABASE_ID,
@@ -38,19 +42,19 @@ export async function getAllPosts(): Promise<NotionPost[]> {
     const id = post.id
 
     // titleプロパティの取り出し
-    const title = post.properties.title.title[0]?.plain_text;
+    const title = post.properties.title.title[0]?.plain_text ?? 'No title';
 
     // multi_selectプロパティの取り出し（例：types）
-    const tags = post.properties.tags.multi_select.map((item: any) => item.name);
+    const tags = post.properties.tags?.multi_select.map((item: any) => item.name) ?? [];
 
     // filesプロパティの取り出し（例：file）
-    const img = post.properties.file.files.map((file: any) => file.file.url);
+    const img = post.properties.img?.files.map((file: any) => file.file.url) ?? [];
 
     // dateプロパティの取り出し
-    const createdAt = post.properties.created_at.date.start;
+    const createdAt = post.properties.created_at?.date?.start ?? 'No created_at';
 
     // peopleプロパティの取り出し（例：author）
-    const author = post.properties.author.select.name;
+    const author = post.properties.author?.select?.name ?? 'Unknown author';
 
     // プロパティをまとめたオブジェクトを返す
     return { id, title, tags, img, createdAt, author };
@@ -59,9 +63,35 @@ export async function getAllPosts(): Promise<NotionPost[]> {
   return postsProperties
 }
 
+/**
+ * ブログ本文の取得
+ * @param pageId 
+ * @returns 
+ */
 export async function getPageContent(pageId: string) {
   const mdblocks = await n2m.pageToMarkdown(pageId, 2);
   console.log("Markdown Content:", mdblocks);
 
   return mdblocks;
+}
+
+interface NotionPostInfo {
+  title: string;
+  createdAt: string;
+  author: string
+};
+
+/**
+ * ブログ詳細の情報取得
+ * @param pageId 
+ * @returns 
+ */
+export async function getPageInfo(pageId: string): Promise<NotionPostInfo> {
+  const response = await notion.pages.retrieve({ page_id: pageId });
+  const pageInfo = response.properties
+
+  const title = pageInfo.title.title[0]?.plain_text
+  const createdAt = pageInfo.created_at.date.start
+  const author = pageInfo.author.select.name
+  return { title, createdAt, author }
 }
